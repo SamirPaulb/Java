11cd686c92599cd228129977322a3979c565fe1f
Sagar Paul <paul.samir.2002@gmail.com>
2021-01-24 22:57:07 +0530

    1st commit of EDC project

---
+++ EDC/EDC_Schema.sql
@@ -0,0 +1,2 @@
+create table lab.edc (id int not null primary key, name varchar(20), mobile varchar(10), email varchar(30));
+select * from lab.edc;
---
+++ EDC/Enrolment.java
@@ -0,0 +1,54 @@
+package edc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Enrolment {
+
+	public static void main(String[] args) {
+		String URL = "jdbc:mysql://localhost:3306/lab";
+		String USER = "root";
+		String PASS = "password";
+		Connection connection = null;
+		String insertQuery = "insert into edc (id, name, mobile, email) values (?, ?, ?, ?)";
+		PreparedStatement preparedStatement = null;
+		int status = 0;
+		try {
+			connection = DriverManager.getConnection(URL, USER, PASS);
+			preparedStatement = connection.prepareStatement(insertQuery);
+			preparedStatement.setInt(1, 2);
+			preparedStatement.setString(2, "Sayan Pal");
+			preparedStatement.setString(3, "7319050309");
+			preparedStatement.setString(4, "pal.sayan.2002@gmail.com");
+			status = preparedStatement.executeUpdate();
+			
+			if(1 == status) {
+				System.out.println("Data Insertion Is Successful.");
+			} else {
+				System.out.println("Data Insertion Is Failed.");
+			}
+		} catch (SQLException e) {
+			System.out.println("Error: "+e.getMessage());
+		} finally {
+			if(null != preparedStatement) {
+				try {
+					preparedStatement.close();
+				} catch (SQLException e) {
+					System.out.println("Error: "+e.getMessage());
+				}
+			}
+			if(null != connection) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					System.out.println("Error: "+e.getMessage());
+				}
+			}
+		}
+
+	}
+
+}
+
